77e21abea8a5eebd6f47f9cc79c507d15d6b7bbc
ksanjayreddy <ksanjayreddy817@gmail.com>
2017-01-06 20:53:23 +0530

    login added

--- server.py
+++ server.py
@@ -1,23 +1,51 @@
 from flask import Flask, render_template, url_for
 from flask import jsonify, request
+from flask import flash, redirect,abort
+import datetime as d
+lastOffTime = d.datetime.now()
+netlitres = 0
+prevtank1 = 10
+diff = 0
+x = 0
 
 app = Flask(__name__)
-tank1data = 10
-tank2data = 30
-current =  "ON"
+tank1data = 40
+tank2data = 40
+current =  "OFF"
 option = ""
+ontime = 0
 
-
-@app.route('/',  methods=['GET'])
+'''@app.route('/',  methods=['GET'])
 def index():
 
     global tank1_level
-    return render_template('index_gauge.html')
+    return render_template('index_gauge.html')  '''
+
+
+@app.route('/')
+def home():
+    lastOffTime = d.datetime.now()
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        return render_template('index_gauge.html')
+    else:
+        return home()
+
 
 @app.route('/deptho/<int:depth_cm1>', methods=['GET'])
 def show_post1(depth_cm1):
     global tank1data
+    global netlitres
+    global prevtank1
+    global diff
+    if depth_cm1 < prevtank1:
+        diff = (prevtank1 - depth_cm1)
     tank1data = depth_cm1
+    prevtank1 = depth_cm1
+    netlitres = netlitres + diff
     return 'ok'
 
 @app.route('/change', methods=['GET','POST'])
@@ -30,18 +58,24 @@ def change():
 
 @app.route('/stat', methods=['GET'])
 def status():
+    global ontime
     global tank1data
     global tank2data
     global current
+
     if tank1data<20 and tank2data>20 and option == "auto":
-        current = "ON"
+        current = "On"
     elif option == "on":
-        current = "ON"
+        current = "On"
     elif option == "off":
         current = "OFF"
-    else :
+    else:
         current = "OFF"
-    return current
+    if current == "On":
+        return 'abc$'
+    else:
+        return 'xyz$'
+
 
 @app.route('/depths/<int:depth_cm2>', methods=['GET'])
 def show_post2(depth_cm2):
@@ -54,7 +88,22 @@ def return_global():
     global tank1data
     global tank2data
     global current
-    return jsonify(tank1 = tank1data , tank2 = tank2data, stat = current)
+    global ontime
+    global lastOffTime
+    global netlitres
+    global x
+    x = x+ 1
+    if current == "OFF":
+        lastOffTime = d.datetime.now()
+    elif current == "On":
+        if  0==0:
+            f= 0
+            diff = d.datetime.now() - lastOffTime
+            lastOffTime = d.datetime.now()
+            f = diff.microseconds
+            ontime = ontime + f
+
+    return jsonify(tank1 = tank1data , tank2 = tank2data, stat = current, time = ontime/1000000, net= netlitres)
 
 
 if __name__ == "__main__":
