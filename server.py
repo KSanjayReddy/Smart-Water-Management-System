9ad1e34838afa7c40273fe43b07e97d692e7fe3a
ksanjayreddy <ksanjayreddy817@gmail.com>
2017-01-20 10:04:59 +0530

    working

--- server.py
+++ server.py
@@ -15,11 +15,15 @@ diff = 0
 x = 0
 logged_in = False
 
+power =0
 tank1data = 40
 tank2data = 40
 current =  "OFF"
 option = ""
 ontime = 0
+stat1 = ""
+stat2 = ""
+stat3 = ""
 
 '''@app.route('/',  methods=['GET'])
 def index():
@@ -52,18 +56,29 @@ def check_login():
         return redirect('/')
     else:
         return render_template('login.html')
+
+
 @app.route('/water', methods=['GET'])
 def water():
     if logged_in:
         return render_template('index_gauge.html')
     else:
         return redirect('/login')
+
+
 @app.route('/energy', methods=['GET'])
 def energy():
     if logged_in:
         return render_template('energy.html')
     else:
         return redirect('/login')
+
+'''@app.route('/power/<int:p>', methods=['GET'])
+def power():
+    global power
+    power = p
+    return 'ok' '''
+
 @app.route('/deptho/<int:depth_cm1>', methods=['GET'])
 def show_post1(depth_cm1):
     global tank1data
@@ -77,6 +92,14 @@ def show_post1(depth_cm1):
     netlitres = netlitres + diff
     return 'ok'
 
+@app.route('/power/<int:p>', methods=['GET'])
+def power(p):
+    global power
+    power = p
+    return 'ok'
+
+
+
 @app.route('/change/<string:switch>', methods=['GET','POST'])
 def change(switch):
     global option
@@ -84,25 +107,54 @@ def change(switch):
     return option
 
 
-@app.route('/stat', methods=['GET'])
-def status():
+@app.route('/stat/<int:p>', methods=['GET'])
+def status(p):
     global ontime
     global tank1data
     global tank2data
     global current
+    global option
+    global stat1,stat2,stat3
+    global power
+    power = p
+    s1 =""
+    s2 =""
+    s3 = ""
 
     if tank1data<20 and tank2data>20 and option == "auto":
+        s1 = "xyz"
+        stat1="a"
         current = "On"
     elif option == "on":
-        current = "On"
+        s2 = "abc"
+        stat2 = "a"
     elif option == "off":
-        current = "OFF"
-    else:
-        current = "OFF"
-    if current == "On":
-        return 'abc$'
-    else:
-        return 'zyx$'
+        s2 = ""
+        stat2 = ""
+    elif option == "a2on":
+        s3 = "pqr"
+        stat3 = "a"
+    elif option == "a2off":
+        s3 = ""
+        stat3 = ""
+    elif option == "a1on":
+        s1 = "xyz"
+        stat1 = "a"
+        current = "On"
+    elif option == "a1off":
+        s1 = ""
+        stat1  = ""
+        current = "Off"
+    pfinal = s1 + s2 + s3
+    if stat1 =="a":
+        pfinal = pfinal + "xyz"
+    if stat2 == "a":
+        pfinal = pfinal + "abc"
+    if stat3 == "a":
+        pfinal = pfinal + "pqr"
+    final = pfinal + "$"
+    return final
+
 
 
 @app.route('/depths/<int:depth_cm2>', methods=['GET'])
@@ -120,6 +172,8 @@ def return_global():
     global lastOffTime
     global netlitres
     global x
+    global power
+
     x = x+ 1
     if current == "OFF":
         lastOffTime = d.datetime.now()
